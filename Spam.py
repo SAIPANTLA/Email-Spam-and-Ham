c8e1f9ad783276ab52405d9c3451d675143fc81b
SAIPANTLA <143243335+SAIPANTLA@users.noreply.github.com>
2024-07-10 16:57:40 +0530

    Spam.py

--- Spam.py
+++ Spam.py
@@ -4,12 +4,12 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 
 # Streamlit app
-st.image(r"C:\Users\SK\Downloads\innomaticslogo.webp")
+st.image(r"innomaticslogo.webp")
 st.title('Email SPAM or HAM Classifier')
 
 # Load the trained model
-model = pickle.load(open(r"C:\Users\SK\Machine Learning\model.pkl", "rb"))
-model1= pickle.load(open(r"C:\Users\SK\Machine Learning\model1.pkl", "rb"))
+model = pickle.load(open(r"model.pkl", "rb"))
+model1= pickle.load(open(r"model1.pkl", "rb"))
 
 # Input text box
 email=st.text_input("Please Enter Your Email text to classify")
@@ -21,8 +21,11 @@ prediction = model.predict(checking)[0]
 if st.button('Predict'):
     if prediction=="spam":
         st.write("This Email is SPAM,Stay SAFE")
-        st.image(r"C:\Users\SK\Downloads\spam.png")
+        st.image(r"spam.png")
     else:
+        st.write("This Email is NOT SPAM,it's SAFE")
+        st.image(r"ham.jpeg")
+
         st.write("This Email is NOT SPAM,it's SAFE")
         st.image(r"C:\Users\SK\Downloads\ham.jpeg")
 
